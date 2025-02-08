be89dc905de3277b0c69745a82f8d0eecb47b7df
Kamal Sunar <kkskgamer@gmail.com>
2025-02-08 12:21:36 +0545

    some minor adjustments

--- frontend/src/components/chat_dashboard_components/message_input_box.jsx
+++ frontend/src/components/chat_dashboard_components/message_input_box.jsx
@@ -1,16 +1,26 @@
-
+import toggler from "../../assets/toggler.svg"
+import { useState } from "react";
 
 
 const Message_input_box = () => {
+    const [arrow_direction, set_arrow_direction] = useState(true) // true = left
+    const toggle = () => {
+        set_arrow_direction(!arrow_direction)
+    }
     return (
         <>
         <div className="Message_input_box bg-stone-800 w-full h-12 flex justify-center items-center">
-            <div className="flex justify-center items-center h-11 w-full">
+            <div className="flex justify-center items-center h-11 w-full ">
+                <div className="list_toggler border-t-2 border-b-2 border-r-2 border-blue-500 bg-blue-300 cursor-pointer hover:bg-blue-500 flex items-center justify-center" style={{padding: "2px"}}>
+                    <button className=" cursor-pointer" onClick={toggle}>
+                        <img className={`w-6 p-1 transform transition-transform duration-300 ${arrow_direction ? "-scale-x-100" : ""}`}src={toggler} alt="" />
+                    </button>
+                </div>
                 <div className="input_box w-full">
                     <input type="text" className="w-full border-2 border-green-500 h-11" placeholder="Enter your message" style={{margin: "4px"}}/>
                 </div>
                 <div className="send_button bg-blue-500 h-9 w-20 flex items-center justify-center rounded-lg" style={{margin: "4px", padding: "2px"}}>
-                    <button className=" cursor-pointe text-3xl font-bold">
+                    <button className="cursor-pointer text-3xl font-bold">
                         Send
                     </button>
                 </div>
--- frontend/src/components/commons/search.jsx
+++ frontend/src/components/commons/search.jsx
@@ -1,18 +1,16 @@
 import search from "../../assets/search.svg"
 
-
 const Search = () => {
     return (
         <>
-            <div className="search w-fit">
-                <div className="flex justify-center items-center" style={{marginLeft: "4px", marginRight: "4px"}}>
+            <div className="search w-full">
+                <div className="flex justify-center items-center w-full bg-stone-800" style={{marginLeft: "4px", marginRight: "4px"}}>
                     <div className="input flex" style={{margin: "4px"}}>
                         <input type="text" className="border-2 border-green-500 rounded" placeholder="Search" />
-
                     </div>
-                    <div className="searh_button">
-                        <button>
-                            <img src={search} alt="" />
+                    <div className="searh_button w-10 flex items-center justify-center h-7 rounded bg-blue-300 cursor-pointer hover:bg-blue-500">
+                        <button className="cursor-pointer">
+                            <img className="w-6" src={search} alt="" />
                         </button>
                     </div>
                 </div>
